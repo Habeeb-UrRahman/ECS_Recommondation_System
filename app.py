ebdb8117f5771721bec108e1057bd885fcf5716d
Habeeb Ur Rahman <rahman.habeeb@outlook.com>
2023-07-29 14:59:56 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
 # Load and preprocess the dataset
-dataset = pd.read_csv('plants_data.csv')  # Replace 'plants_data.csv' with the correct path to your dataset
+dataset = pd.read_csv('plant_dataset2.csv')  # Replace 'plants_data.csv' with the correct path to your dataset
 # Preprocess the dataset as needed (e.g., handle missing values, encode categorical variables, etc.)
 
 # Split the dataset into training and testing sets
@@ -19,35 +19,27 @@ model.fit(X_train, y_train)
 # Define the Streamlit app
 def main():
     st.markdown("<h1>VIT-AP UNIVERSITY ECS PROJECT</h1w>", unsafe_allow_html=True)
-    st.title('Soil Recommondation System')
+    st.title('Soil Recommendation System')
     st.write('Enter the type of plants you want to grow and get recommendations on the attributes like Turbidity, Ph, Nitrogen level, Phosphorous level, Potassium level, Humidity and Rainfall which determine the health of the soil and the type of environment best suitable for your plants.')
+
+    # Get the list of unique plant names from the dataset
+    plant_names = dataset['label'].unique()
+
     # Accept user input for the number of plants and their names
     num_plants = st.number_input("SELECT THE NUMBER OF PLANTS", min_value=1, value=1, step=1)
-    plant_names = []
+
     selected_plant_attributes = {}
-
     for i in range(num_plants):
-        plant_name = st.text_input(f"PLANT {i+1} NAME:")
-        plant_names.append(plant_name)
-
-        # Find the row for the selected plant
-        if plant_name in dataset['label'].unique():
-            plant_row = dataset[dataset['label'] == plant_name].iloc[0]
-            selected_plant_attributes[plant_name] = plant_row.drop('label')
-        else:
-            st.warning(f"Plant '{plant_name}' is not a valid input here.")
+        plant_name = st.selectbox(f"PLANT {i+1} NAME:", plant_names)
+        selected_plant_attributes[plant_name] = dataset[dataset['label'] == plant_name].drop('label', axis=1).iloc[0]
 
     # Generate recommendations for each plant
     recommendations = {}
     for plant_name, plant_attributes in selected_plant_attributes.items():
-        if plant_name in dataset['label'].unique():
-            # Convert plant_attributes to DataFrame to preserve feature names
-            plant_attributes_df = pd.DataFrame([plant_attributes], columns=X.columns)
-            predicted_label = model.predict(plant_attributes_df)[0]
-            plant_recommendations = dataset[dataset['label'] == predicted_label].iloc[0].drop('label')
-            recommendations[plant_name] = plant_recommendations
-        else:
-            st.warning(f"Plant '{plant_name}' is not a valid input here.")
+        plant_attributes_df = pd.DataFrame([plant_attributes])
+        predicted_label = model.predict(plant_attributes_df)[0]
+        plant_recommendations = dataset[dataset['label'] == predicted_label].drop('label', axis=1).iloc[0]
+        recommendations[plant_name] = plant_recommendations
 
     # Display the recommendations
     st.header("Recommendations:")
@@ -63,10 +55,9 @@ def main():
     for attribute in dataset.columns[1:]:
         attribute_values = []
 
-        for plant_name in plant_names:
-            if plant_name in selected_plant_attributes:
-                attribute_value = selected_plant_attributes[plant_name][attribute]
-                attribute_values.append(attribute_value)
+        for plant_name in selected_plant_attributes:
+            attribute_value = selected_plant_attributes[plant_name][attribute]
+            attribute_values.append(attribute_value)
 
         if len(set(attribute_values)) == 1:
             optimal_value = attribute_values[0]
@@ -76,13 +67,12 @@ def main():
 
         optimal_attributes[attribute] = optimal_value
 
-        for plant_name in plant_names:
-            if plant_name in selected_plant_attributes:
-                plant_table = pd.DataFrame({attribute: attribute_values})
-                if plant_name in plant_tables:
-                    plant_tables[plant_name][attribute] = attribute_values
-                else:
-                    plant_tables[plant_name] = plant_table
+        for plant_name in selected_plant_attributes:
+            plant_table = pd.DataFrame({attribute: attribute_values})
+            if plant_name in plant_tables:
+                plant_tables[plant_name][attribute] = attribute_values
+            else:
+                plant_tables[plant_name] = plant_table
 
     # Display the optimal attribute values
     st.header("Optimal Attribute Values for Selected Plants:")
@@ -94,8 +84,8 @@ def main():
         st.header("Plants that cannot be grown together due to conflicting attribute values:")
         for attribute, values in conflicting_values.items():
             st.subheader(f"Attribute: {attribute}")
-            conflicting_plants = [plant_name for plant_name in plant_names if selected_plant_attributes.get(plant_name, {}).get(attribute) in values]
-            conflicting_plant_tables = {plant: plant_tables.get(plant) for plant in conflicting_plants}
+            conflicting_plants = [plant_name for plant_name in selected_plant_attributes if selected_plant_attributes[plant_name][attribute] in values]
+            conflicting_plant_tables = {plant: plant_tables[plant] for plant in conflicting_plants}
             for plant, table in conflicting_plant_tables.items():
                 if table is not None:
                     st.write(f"Plant: {plant}")
@@ -105,7 +95,6 @@ def main():
 if __name__ == "__main__":
     main()
 
-print("\n\n")
 # Team details
 st.markdown("<h3>TEAM DETAILS:</h3>", unsafe_allow_html=True)
 st.markdown("<p>Habeeb Ur Rahman - 21BCE7005</p>", unsafe_allow_html=True)
